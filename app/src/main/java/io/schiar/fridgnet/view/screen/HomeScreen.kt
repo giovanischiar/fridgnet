38dd49f534b284b205a92f62a3a0fe1f640e85d0
Giovani Schiar <giovanischiar@gmail.com>
2023-04-28 15:29:59 -0700

    Create a ViewModel State to Send All Images to Home, Showing Only The Images in a Grid Sorted by Most Recent

--- app/src/main/java/io/schiar/fridgnet/view/screen/HomeScreen.kt
+++ app/src/main/java/io/schiar/fridgnet/view/screen/HomeScreen.kt
@@ -1,14 +1,15 @@
 package io.schiar.fridgnet.view.screen
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import coil.compose.AsyncImage
 import io.schiar.fridgnet.view.PhotoPicker
 import io.schiar.fridgnet.view.viewdata.ImageViewData
@@ -21,7 +22,7 @@ fun HomeScreen(viewModel: MainViewModel) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val images by viewModel.visibleImages.collectAsState()
+        val images by viewModel.allImages.collectAsState()
         val (photoPickerShowing, isPhotoPickerShowing) = remember { mutableStateOf(false) }
         Button( onClick = { isPhotoPickerShowing.invoke(true) }) {
             Text("Add Photos")
@@ -37,25 +38,17 @@ fun HomeScreen(viewModel: MainViewModel) {
 }
 @Composable
 fun Photos(images: List<ImageViewData>) {
-    LazyColumn {
-        items(images) { image ->
-            val (uri, date, location) = image
-            val (lat, lng) = location
-            Column {
+    val sortedImages = images.sortedBy { it.date }.reversed()
+    LazyVerticalGrid(columns = GridCells.Fixed(4)) {
+        items(images.size) { index ->
+            val (uri) = sortedImages[index]
+            Column(modifier = Modifier.size(Dp(value = 100f))) {
                 AsyncImage(
                     model = uri,
                     contentDescription = null,
                     modifier = Modifier.fillMaxWidth(),
                     contentScale = ContentScale.Crop
                 )
-                Row {
-                    Text("Date:")
-                    Text(date)
-                }
-                Row {
-                    Text("Location:")
-                    Text("($lat, $lng)")
-                }
             }
         }
     }
--- app/src/main/java/io/schiar/fridgnet/viewmodel/MainViewModel.kt
+++ app/src/main/java/io/schiar/fridgnet/viewmodel/MainViewModel.kt
@@ -15,11 +15,14 @@ class MainViewModel: ViewModel() {
     private var _images: List<Image> = emptyList()
     private val _visibleImages = MutableStateFlow(value = _images.toViewData())
     val visibleImages: StateFlow<List<ImageViewData>> = _visibleImages.asStateFlow()
+    private val _allImages = MutableStateFlow(value = _images.toViewData())
+    val allImages: StateFlow<List<ImageViewData>> = _allImages.asStateFlow()
 
     fun addImage(uri: String, date: Long, latitude: Double, longitude: Double) {
         val newLocation = Location(latitude = latitude, longitude = longitude)
         val newImage = Image(uri = uri, date = date, location = newLocation)
         _images = _images + newImage
+        _allImages.update { _images.toViewData() }
     }
 
     fun visibleAreaChanged(bounds: LatLngBounds?) {
