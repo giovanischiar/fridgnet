9397cd04145ae56555425e223cd4f73a3fa93cba
Giovani Schiar <giovanischiar@gmail.com>
2023-05-20 05:01:57 -0700

    Create Tests For Polygon Class. Fix findBoundingBox Method to Handle Cross Antimeridian Polygons, Permit Creating Int Coordinates.

--- app/src/main/java/io/schiar/fridgnet/model/Coordinate.kt
+++ app/src/main/java/io/schiar/fridgnet/model/Coordinate.kt
@@ -3,6 +3,9 @@ package io.schiar.fridgnet.model
 import kotlin.math.abs
 
 data class Coordinate(val latitude: Double, val longitude: Double) {
+    constructor(latitude: Int, longitude: Int) :
+            this(latitude = latitude.toDouble(), longitude = longitude.toDouble())
+
     fun wasAntimeridianCrossed(other: Double): Boolean {
         val (_, longitude) = this
         return (longitude > 0.0 && other < 0.0 || longitude < 0.0 && other > 0.0) &&
--- app/src/main/java/io/schiar/fridgnet/model/Polygon.kt
+++ app/src/main/java/io/schiar/fridgnet/model/Polygon.kt
@@ -7,6 +7,9 @@ data class Polygon(val coordinates: List<Coordinate>) {
         var minLatitude = Double.POSITIVE_INFINITY
         var minLongitude = Double.POSITIVE_INFINITY
 
+        var furtherEastAwayFromAntimeridianLongitude = -180.0
+        var furtherWestAwayFromAntimeridianLongitude = 180.0
+
         var wasAntimeridianEverCrossed = false
 
         for (i in this.coordinates.indices) {
@@ -14,12 +17,19 @@ data class Polygon(val coordinates: List<Coordinate>) {
             val (latitude, longitude) = coordinate
 
             if (latitude > maxLatitude) { maxLatitude = latitude }
-
             if (longitude > maxLongitude) { maxLongitude = longitude }
-
             if (latitude < minLatitude) { minLatitude = latitude }
-
             if (longitude < minLongitude) { minLongitude = longitude }
+
+            if (wasAntimeridianEverCrossed) {
+                if (longitude < 0 && longitude > furtherEastAwayFromAntimeridianLongitude) {
+                    furtherEastAwayFromAntimeridianLongitude = longitude
+                }
+
+                if (longitude > 0 && longitude < furtherWestAwayFromAntimeridianLongitude) {
+                    furtherWestAwayFromAntimeridianLongitude = longitude
+                }
+            }
 
             if (i+1 < this.coordinates.size) {
                 val next = this.coordinates[i+1]
@@ -29,9 +39,17 @@ data class Polygon(val coordinates: List<Coordinate>) {
         }
 
         val southwestLatitude = minLatitude
-        val southwestLongitude = if (wasAntimeridianEverCrossed) maxLongitude else minLongitude
+        val southwestLongitude = if (!wasAntimeridianEverCrossed) {
+            minLongitude
+        } else {
+            furtherWestAwayFromAntimeridianLongitude
+        }
         val northeastLatitude = maxLatitude
-        val northeastLongitude = if (wasAntimeridianEverCrossed) minLongitude else maxLongitude
+        val northeastLongitude = if (!wasAntimeridianEverCrossed) {
+            maxLongitude
+        } else {
+            furtherEastAwayFromAntimeridianLongitude
+        }
 
         return BoundingBox(
             southwest = Coordinate(latitude = southwestLatitude, longitude = southwestLongitude),
