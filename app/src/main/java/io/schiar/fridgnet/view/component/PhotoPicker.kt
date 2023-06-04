4f41edd2525641c11f397822f04cd8b1455d4307
Giovani Schiar <giovanischiar@gmail.com>
2023-06-04 14:36:11 -0700

    Process All Uris in a Separate Thread

--- app/src/main/java/io/schiar/fridgnet/view/component/PhotoPicker.kt
+++ app/src/main/java/io/schiar/fridgnet/view/component/PhotoPicker.kt
@@ -5,22 +5,28 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.exifinterface.media.ExifInterface
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun PhotoPicker(onPhotosPicked: (uri: String, date: Long, latitude: Double, longitude: Double) -> Unit) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
     val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetMultipleContents(),
         onResult = { uris ->
-            uris.forEach { uri ->
-                context.contentResolver.openInputStream(uri)!!.use { ins ->
-                    val exifInterface = ExifInterface(ins)
-                    val latLng = exifInterface.latLong ?: doubleArrayOf(0.0, 0.0)
-                    @SuppressLint("RestrictedApi")
-                    val date = exifInterface.dateTime
-                    onPhotosPicked(uri.toString(), date ?: 0L, latLng[0], latLng[1])
+            coroutineScope.launch(Dispatchers.IO) {
+                uris.forEach { uri ->
+                    context.contentResolver.openInputStream(uri)!!.use { ins ->
+                        val exifInterface = ExifInterface(ins)
+                        val latLng = exifInterface.latLong ?: doubleArrayOf(0.0, 0.0)
+                        @SuppressLint("RestrictedApi")
+                        val date = exifInterface.dateTime
+                        onPhotosPicked(uri.toString(), date ?: 0L, latLng[0], latLng[1])
+                    }
                 }
             }
         }
