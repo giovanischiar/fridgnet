b6f43bb393458b48cf03475ff4a1139eb68e6577
Giovani Schiar <giovanischiar@gmail.com>
2023-06-11 12:16:40 -0700

    Handle Brazil Localities, The Locality Doesn't Exist Being Replaced By Its SubAdminArea

--- app/src/main/java/io/schiar/fridgnet/model/repository/address/AddressToModelExtensions.kt
+++ app/src/main/java/io/schiar/fridgnet/model/repository/address/AddressToModelExtensions.kt
@@ -5,7 +5,7 @@ import android.location.Address as AndroidAddress
 
 fun AndroidAddress.toModelAddress(): Address {
     return Address(
-        locality = locality,
+        locality = if (countryName != "Brazil") locality else subAdminArea,
         subAdminArea = subAdminArea,
         adminArea = adminArea,
         countryName = countryName
