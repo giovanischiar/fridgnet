dfc02eec868a62ba644d8bf634487bcc86173a02
Giovani Schiar <giovanischiar@gmail.com>
2023-06-14 11:38:11 -0700

    Refactoring Polygon Searcher

--- app/src/main/java/io/schiar/fridgnet/model/datasource/nominatim/PolygonSearcher.kt
+++ app/src/main/java/io/schiar/fridgnet/model/datasource/nominatim/PolygonSearcher.kt
@@ -1,42 +1,25 @@
 package io.schiar.fridgnet.model.datasource.nominatim
 
 import io.schiar.fridgnet.Log
-import io.schiar.fridgnet.model.Address
 import retrofit2.Response
 
-class PolygonSearcher() {
-    suspend fun search(address: Address): Response<List<Result<GeoJsonAttributes>>> {
-        val quotesApi = RetrofitHelper.getInstance().create(NominatimApi::class.java)
-        return if(
-            address.locality != null &&
-            address.subAdminArea != null &&
-            address.adminArea != null &&
-            address.countryName != null
-        ) {
-            quotesApi.getResultsCity(
-                city = address.locality,
-                state = address.adminArea,
-                country = address.countryName
-            )
-        } else {
-            val name = address.name()
-            Log.d("api result", "searching $name")
-            quotesApi.getResults(q = name)
-        }
-    }
+typealias APIResponse = Response<List<Result<GeoJsonAttributes>>>
 
-    suspend fun searchCity(city: String, state: String, country: String): Response<List<Result<GeoJsonAttributes>>> {
+class PolygonSearcher {
+    suspend fun searchCity(city: String, state: String, country: String): APIResponse {
         val quotesApi = RetrofitHelper.getInstance().create(NominatimApi::class.java)
         val result = quotesApi.getResultsCity(city = city, state = state, country = country)
         val bodies = result.body() ?: return result
         val body = if (bodies.isEmpty()) return result else bodies[0]
-        val geojson = body.geojson
-        val type = geojson.type
+        val geoJSON = body.geojson
+        val type = geoJSON.type
 
         if (type == "Polygon") {
             if (bodies.size == 1) return result
             val secondBody = bodies[1]
-            if (secondBody.display_name == body.display_name && secondBody.type == "administrative") {
+            if (
+                secondBody.display_name == body.display_name && secondBody.type == "administrative"
+            ) {
                 if (secondBody.geojson.type == "MultiPolygon") {
                     return Response.success(listOf(secondBody))
                 }
@@ -49,8 +32,13 @@ class PolygonSearcher() {
                 return quotesApi.getResults(q = "$city, $state, $country")
             }
             val secondBody =  bodies[1]
-            Log.d("Search for API Polygon", "Second Result name: {$secondBody.name} type: {$secondBody.type}")
-            if (secondBody.display_name == body.display_name && secondBody.type == "administrative") {
+            Log.d(
+                "Search for API Polygon",
+                "Second Result name: {$secondBody.name} type: {$secondBody.type}"
+            )
+            if (
+                secondBody.display_name == body.display_name && secondBody.type == "administrative"
+            ) {
                 Log.d("Search for API Polygon", "Second body is the administrative")
                 return Response.success(listOf(secondBody))
             }
@@ -60,17 +48,17 @@ class PolygonSearcher() {
         return result
     }
 
-    suspend fun searchCounty(county: String, state: String, country: String): Response<List<Result<GeoJsonAttributes>>> {
+    suspend fun searchCounty(county: String, state: String, country: String): APIResponse {
         val quotesApi = RetrofitHelper.getInstance().create(NominatimApi::class.java)
         return quotesApi.getResultsCounty(county = county, state = state, country = country)
     }
 
-    suspend fun searchState(state: String, country: String): Response<List<Result<GeoJsonAttributes>>> {
+    suspend fun searchState(state: String, country: String): APIResponse {
         val quotesApi = RetrofitHelper.getInstance().create(NominatimApi::class.java)
         return quotesApi.getResultsState(state = state, country = country)
     }
 
-    suspend fun searchCountry(country: String): Response<List<Result<GeoJsonAttributes>>> {
+    suspend fun searchCountry(country: String): APIResponse {
         val quotesApi = RetrofitHelper.getInstance().create(NominatimApi::class.java)
         return quotesApi.getResultsCountry(country = country)
     }
