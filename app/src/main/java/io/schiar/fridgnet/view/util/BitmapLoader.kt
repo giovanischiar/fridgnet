d64e0d786d000fc922128b33980f7fd911356ed5
Giovani Schiar <giovanischiar@gmail.com>
2023-06-14 17:31:11 -0700

    Handle Without Permission Exception. to Open Bitmap Files it is Needed to Ask Permission

--- app/src/main/java/io/schiar/fridgnet/view/util/BitmapLoader.kt
+++ app/src/main/java/io/schiar/fridgnet/view/util/BitmapLoader.kt
@@ -6,18 +6,24 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import io.schiar.fridgnet.Log
 import kotlin.math.roundToInt
 
 class BitmapLoader(private val contentResolver: ContentResolver, private val uri: Uri) {
     fun convert(): BitmapDescriptor? {
-        val options = BitmapFactory.Options()
-        options.inSampleSize = 4
-        val inputStream = contentResolver.openInputStream(uri)
-        val rawBitmap = BitmapFactory.decodeStream(
-            inputStream,
-            null,
-            options
-        ) ?: return null
+        val rawBitmap = try {
+            val options = BitmapFactory.Options()
+            options.inSampleSize = 4
+            val inputStream = contentResolver.openInputStream(uri)
+            BitmapFactory.decodeStream(
+                inputStream,
+                null,
+                options
+            )
+        } catch (e: Exception) {
+            Log.d("Bitmap Loader", "Exception! $e")
+            null
+        } ?: return null
         val bitmap = resize(bitmap = rawBitmap)
         return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
