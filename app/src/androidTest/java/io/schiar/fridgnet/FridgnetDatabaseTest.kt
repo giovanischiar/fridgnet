d4dc87444fb228b44649527fdd556a9c77cdf829
Giovani Schiar <giovanischiar@gmail.com>
2024-04-05 14:14:03 -0700

    Fix Android Test

--- app/src/androidTest/java/io/schiar/fridgnet/FridgnetDatabaseTest.kt
+++ app/src/androidTest/java/io/schiar/fridgnet/FridgnetDatabaseTest.kt
@@ -5,9 +5,11 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.schiar.fridgnet.library.room.AdministrativeUnitNameRoomDataSource
 import io.schiar.fridgnet.library.room.CartographicBoundaryRoomDataSource
 import io.schiar.fridgnet.library.room.FridgnetDatabase
 import io.schiar.fridgnet.model.*
+import io.schiar.fridgnet.model.datasource.AdministrativeUnitNameDataSource
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.equalTo
@@ -21,6 +23,7 @@ import java.io.IOException
 class FridgnetDatabaseTest {
     private lateinit var database: FridgnetDatabase
     private lateinit var cartographicBoundaryRoomService: CartographicBoundaryRoomDataSource
+    private lateinit var administrativeUnitNameDataSource: AdministrativeUnitNameDataSource
 
     @Before
     fun createDatabase() {
@@ -32,6 +35,9 @@ class FridgnetDatabaseTest {
         cartographicBoundaryRoomService = CartographicBoundaryRoomDataSource(
             cartographicBoundaryDAO = database.cartographicBoundaryDAO()
         )
+        administrativeUnitNameDataSource = AdministrativeUnitNameRoomDataSource(
+            administrativeUnitNameDAO = database.administrativeUnitNameDAO()
+        )
     }
 
     @After
@@ -42,36 +48,6 @@ class FridgnetDatabaseTest {
 
     @Test
     fun insertCartographicBoundaryAndEnsureTheCartographicBoundaryRetrievedIsTheSame() = runTest {
-        val region = Region(
-            polygon = Polygon(
-                geoLocations = listOf(
-                    GeoLocation(latitude = -10, longitude = -10),
-                    GeoLocation(latitude = -10, longitude = 10),
-                    GeoLocation(latitude = 10, longitude = 10),
-                    GeoLocation(latitude = 10, longitude = -10)
-                )
-            ),
-
-            holes = listOf(
-                Polygon(
-                    geoLocations = listOf(
-                        GeoLocation(latitude = -5, longitude = -5),
-                        GeoLocation(latitude = -5, longitude = 5),
-                        GeoLocation(latitude = 5, longitude = 5),
-                        GeoLocation(latitude = 5, longitude = -5)
-                    )
-                )
-            ),
-
-            active = true,
-
-            boundingBox = BoundingBox(
-                southwest = GeoLocation(latitude = -10, longitude = -10),
-                northeast = GeoLocation(latitude = 10, longitude = 10)
-            ),
-            zIndex = 1.0f
-        )
-
         val administrativeUnitName = AdministrativeUnitName(
             locality = "Donut Square",
             subAdminArea = "Square County",
@@ -79,9 +55,49 @@ class FridgnetDatabaseTest {
             countryName = "Square Country",
         )
 
+        administrativeUnitNameDataSource.create(
+            geoLocation = GeoLocation(latitude = 0.0, longitude = 0.0),
+            administrativeUnitName
+        )
+
+        val (storedAdministrativeUnitNameDataSource) = administrativeUnitNameDataSource
+            .retrieveAdministrativeUnitNameWithExistentCartographicBoundaries()
+            .first()
+
         val cartographicBoundary = CartographicBoundary(
-            administrativeUnitName = administrativeUnitName,
-            regions = listOf(region),
+            administrativeUnitName = storedAdministrativeUnitNameDataSource,
+            regions = listOf(
+                Region(
+                    polygon = Polygon(
+                        geoLocations = listOf(
+                            GeoLocation(latitude = -10, longitude = -10),
+                            GeoLocation(latitude = -10, longitude = 10),
+                            GeoLocation(latitude = 10, longitude = 10),
+                            GeoLocation(latitude = 10, longitude = -10)
+                        )
+                    ),
+
+                    holes = listOf(
+                        Polygon(
+                            geoLocations = listOf(
+                                GeoLocation(latitude = -5, longitude = -5),
+                                GeoLocation(latitude = -5, longitude = 5),
+                                GeoLocation(latitude = 5, longitude = 5),
+                                GeoLocation(latitude = 5, longitude = -5)
+                            )
+                        )
+                    ),
+
+                    active = true,
+
+                    boundingBox = BoundingBox(
+                        southwest = GeoLocation(latitude = -10, longitude = -10),
+                        northeast = GeoLocation(latitude = 10, longitude = 10)
+                    ),
+
+                    zIndex = 1.0f
+                )
+            ),
             boundingBox = BoundingBox(
                 southwest = GeoLocation(latitude = -10, longitude = -10),
                 northeast = GeoLocation(latitude = 10, longitude = 10)
@@ -91,9 +107,34 @@ class FridgnetDatabaseTest {
         )
 
         cartographicBoundaryRoomService.create(cartographicBoundary = cartographicBoundary)
+
         val actual = cartographicBoundaryRoomService
-            .selectCartographicBoundaryByAdministrativeUnitName(administrativeUnitName = administrativeUnitName)
+            .selectCartographicBoundaryByAdministrativeUnitName(
+                administrativeUnitName = storedAdministrativeUnitNameDataSource
+            )
             .first()
-        assertThat(actual, equalTo(cartographicBoundary))
+
+        assertThat(actual, equalTo(
+            cartographicBoundary.copy(
+                id = actual?.id ?: 0,
+                regions = cartographicBoundary.regions.mapIndexed { index, region ->
+                    val regionID = actual?.regions?.getOrNull(index)?.id ?: 0
+                    val polygonID = actual?.regions?.getOrNull(index)?.polygon?.id ?: 0
+                    region.copy(
+                        id = regionID,
+                        polygon = region.polygon.copy(id = polygonID),
+                        holes = region.holes.mapIndexed { holeIndex, hole ->
+                            val holeID = actual
+                                ?.regions
+                                ?.getOrNull(index)
+                                ?.holes
+                                ?.getOrNull(holeIndex)
+                                ?.id ?: 0
+                            hole.copy(id = holeID)
+                        }
+                    )
+                }
+            )
+        ))
     }
 }
